dc0c5441c3cb9fc63dc201fbdc880df02ec2d6fc
Jonathan Reynes <joreynes@microsoft.com>
2020-10-23 14:53:33 -0400

    fm

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,7 +5,7 @@
 # Provider block -> please make sure you have added partner id
 provider "azurerm" {
   # Whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "~> 2.20.0"
+  version         = "~> 2.20.0"
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
   client_id       = var.client_id
@@ -26,5 +26,5 @@ provider "azurerm" {
 # Set the terraform backend
 terraform {
   required_version = "~> 0.12.20"
-    backend "azurerm" {} #Backend variables are initialized through the secret and variable folders
+  backend "azurerm" {} #Backend variables are initialized through the secret and variable folders
 }
