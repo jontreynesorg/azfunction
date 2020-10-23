cb343a17b135806a0269055fb7a2886502faaed4
Jonathan Reynes <19974635+jontreynes@users.noreply.github.com>
2020-10-23 12:57:35 -0400

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,10 +6,10 @@
 provider "azurerm" {
   # Whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "~> 2.20.0"
-  # tenant_id       = var.tenant_id
-  # subscription_id = var.subscription_id
-  # client_id       = var.client_id
-  # client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 
   features {}
 }
@@ -26,6 +26,5 @@ provider "azurerm" {
 # Set the terraform backend
 terraform {
   required_version = "~> 0.12.20"
-  backend "local" {} #Backend variables are initialized through the secret and variable folders
-  #   backend "azurerm" {} #Backend variables are initialized through the secret and variable folders
+    backend "azurerm" {} #Backend variables are initialized through the secret and variable folders
 }
